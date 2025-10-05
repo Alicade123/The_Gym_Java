e66d4869b153642d0603e84d6bb0ed4225d60bac
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-10-05 22:10:03 +0200

    different datatyes in java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public  class Main{
     public static void main(String[] args) {
         System.out.print("Hello world, We are in KGL");
+        int num = 12;
+        float f = 12.1f;
+        double d = 12.1;
+        long l = 1_200_000L;
+        System.out.println(l);
     }
+
 }
