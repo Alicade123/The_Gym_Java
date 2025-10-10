9557e5e2083c6564372c4b343716f0dbc60ea69e
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-10-10 10:27:18 +0200

    The second Kata 8kyu: Area or Parameter

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
 
 public class Solution {
+     static void main() {
+        int x= 3;
+        int y = 2;
+        System.out.println(areaOrPerimeter(x,y));
+    }
+    public static int areaOrPerimeter (int l, int w) {
+        // code away...
+        if (l==w) {
+            return l*w;
+        }else{
+            return 2*l+2*w;
+        }
+    }
 }
